4904555e2f22bbc3040855ac833bfa02839fe444
FilkinaGramota <tfilonets@gmail.com>
2018-09-30 13:40:18 +0300

    Add first migration CreateOrderTable

---
+++ TestEFCore/Migrations/20180930102744_CreateOrderTable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TestEFCore.Migrations
+{
+    public partial class CreateOrderTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    OrderStart = table.Column<DateTime>(nullable: false),
+                    Days = table.Column<int>(nullable: false),
+                    OrderEnd = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
