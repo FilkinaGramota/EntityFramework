ebc17d4570c80b7c37628a00e07f9b37c2f9b3ee
FilkinaGramota <tfilonets@gmail.com>
2018-10-03 23:34:05 +0300

    Add class Film (= 1 cassette)

---
+++ TestEFCore/Film.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestEFCore
+{
+    public class Film
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        // many-to-many
+        public IList<FilmOrder> FilmOrders { get; set; }
+
+        // default title
+        public Film()
+        {
+            Title = "Default Title";
+            FilmOrders = new List<FilmOrder>();
+        }
+
+        public Film(string title)
+        {
+            Title = title;
+            FilmOrders = new List<FilmOrder>();
+        }
+    }
+}
